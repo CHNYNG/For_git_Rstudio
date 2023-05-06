77db220076c49b17cf9d3854846191569c10b7a3
CHNYNG <652059698@qq.com>
2023-05-06 21:39:27 +0800

    commit

--- Krige_for_the_soil.R
+++ Krige_for_the_soil.R
@@ -4,6 +4,7 @@ library(ggplot2)
 
 # 读取土壤样本数据
 HSD_env <- read.csv("E:/Chu Lab/HSD.origin/soil_origin.csv",header = T,fileEncoding = "GBK")
+HSD_env_sp <- SpatialPointsDataFrame(coords=HSD_env[,c("gx", "gy")], data=HSD_env)
 soil_data <- read.csv("soil_data.csv")  # 假设你的土壤样本数据保存在名为 "soil_data.csv" 的csv文件中
 
 # 创建空间数据框（SpatialPointsDataFrame）
