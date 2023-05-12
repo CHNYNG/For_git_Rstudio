f83fed65fdecc1c4bbdf92ae060c4151b7fda472
CHNYNG <652059698@qq.com>
2023-05-12 17:27:37 +0800

    5.12

--- colonization_analysis.R
+++ colonization_analysis.R
@@ -7,41 +7,43 @@ load("E:/黑石顶测菌根/菌根侵染率/数据整理/tmp/For_git_Rstudio/roo
 ###计算一下乔木生长速率
 d$gr_rate <- (log(d$DBH2)-log(d$DBH1))
 
-
-library(ggplot2)
+####筛选出AM的个体
+d_AM <- subset(d, !is.na(qr_AM))
 #对样本总体分析
 #####
 #1.相关性分析
-######
-# 画散点图和回归线
-library(dplyr)
 
 ####进行boxcox转换
 library(MASS)
 # 对d_AM$qr_AM列使用Box-Cox转换
-d_AM$qr_AM_bc <- boxcox(as.data.frame(d_AM$qr_AM ~ 1))
-
 boxcox(qr_AM ~ 1, data = d_AM,lambda = seq(4,5, length.out = 10))
 
+#画出转换后的形状
 hist(d_AM$qr_AM^(4.6))
+#进行lm
 r2=lm(gr_rate~qr_AM^4.6, data=d_AM)
+#画出残差的分布
 hist(r2$residuals)
 summary(r2)
 
-a = (d_AM$qr_AM)^4.6
+#画出一列变换后的数据，方便画图
+d_AM$AM_4.6 = (d_AM$qr_AM)^4.6
 
-plot(d_AM$gr_rate~a)
+###使用ggplot2画图
+plot(d_AM$gr_rate~d_AM$AM_4.6)
 
-hist(d_AM$gr_rate)
+####试一下结果的变化
 cor.test(d_AM$qr_AM, d_AM$gr_rate)
-d_AM <- subset(d, !is.na(qr_AM))
+cor.test(d_AM$AM_4.6, d_AM$gr_rate)
 
 
 # 拟合二项式回归模型
+
 fit <- glm(gr_rate ~ qr_AM, data = d_AM, family = binomial(link = "logit"))
 
 # 绘制散点图和拟合曲线
-ggplot(dat, aes(x = qr_AM, y = y)) +
+library(ggplot2)
+ggplot(d, aes(x = qr_AM, y = gr_rate)) +
   geom_point() +
   geom_smooth(method = "glm", method.args = list(family = binomial), se = FALSE) +
   ggtitle(paste0("Logistic Regression (AIC = ", round(AIC(fit), 2), ")"))
